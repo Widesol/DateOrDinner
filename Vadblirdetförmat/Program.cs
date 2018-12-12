7b6283b1f7c68139e91929f231c1c518acd25a70
Widesol <olofwidestrom@hotmail.com>
2018-12-12 14:39:16 +0100

    no message

--- Vadblirdetförmat/Program.cs
+++ Vadblirdetförmat/Program.cs
@@ -6,7 +6,113 @@ namespace Vadblirdetförmat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartApp(); //Välkomsthälsning
+            ReadTextFile();
+            EnterFoodDate(); // Användaren matar in datum 
+            ShowDinnerPlaces(); // Användaren får alternativ på var maten
+            ShowProteinSources();
+            ShowCarbSources();
+            ShowMenues();
+            ShowRecepies();
+            EndOfProgram();
+
         }
+
+      
+        private static void StartApp()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ReadTextFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EnterFoodDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowDinnerPlaces()
+        {
+            if (false)
+            {
+                GetHolidayDinner();
+            }
+            else if(false)
+            {
+                ShowPlaces();
+            }
+            else if (false)
+            {
+                ShowPlaces();
+            }
+            else if (false)
+            {
+                ShowPlaces();
+            }
+            else
+            {
+                ShowPlaces();
+            }
+        }
+
+        private static void ShowPlaces()
+        {
+            //var showPlaces = ReadTextFil.Select(x => x.Place).Distinct();
+            PrintChoices(showPlaces);
+            EnterChoice();
+        }
+
+
+
+        private static void GetHolidayDinner()
+        {
+            if (true)
+            {
+                ShowPlaces();
+                //Om det är Jul, Påsk eller Midsommar.
+
+            }
+        }
+
+        private static void ShowProteinSources()
+        {
+            EnterChoice();
+            throw new NotImplementedException();
+        }
+
+        private static void EnterChoice()
+        {
+            if(3)
+
+            
+            throw new NotImplementedException();
+        }
+
+        private static void ShowCarbSources()
+        {
+            EnterChoice();
+            throw new NotImplementedException();
+        }
+        private static void ShowMenues()
+        {
+            CreateNewMenue();
+        }
+        private static void ShowRecepies()
+        {
+            throw new NotImplementedException();
+        }
+        private static void PrintChoices(object showPlaces)
+        {
+            throw new NotImplementedException();
+        }
+        private static void EndOfProgram()
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
 }
